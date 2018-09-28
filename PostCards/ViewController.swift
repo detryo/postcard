5d7c9564d30f52300047774d9af003274ce8a29a
Cristian Arenas <cristiansedanoarenas@macbook-pro-de-cristian.local>
2018-09-28 16:54:26 +0100

    Initial Commit

---
+++ PostCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostCards
+//
+//  Created by Cristian Sedano Arenas on 28/9/18.
+//  Copyright © 2018 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
