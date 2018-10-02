918793d1dd31c52ed716fd926707a11174a14923
Cristian Sedano Arenas <cristiansedanoarenas@MacBook-Pro-de-Cristian.local>
2018-10-02 16:55:03 +0100

    Collection View Data Source
    Implement UIColor and some colors, fill func collectionView

--- PostCards/Controllers/ViewController.swift
+++ PostCards/Controllers/ViewController.swift
@@ -13,16 +13,43 @@ class ViewController: UIViewController, UICollectionViewDataSource, UITableViewD
     @IBOutlet weak var postcardImageView: UIImageView!
     @IBOutlet weak var colorCollectionView: UICollectionView!
     
+    var colors = [UIColor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.colors += [.black, .gray, .white, .red, .orange, .yellow,
+                        .green, .cyan, .blue, .purple, .magenta]
+        
+        for hue in 0...9{
+            for sat in 1...10{
+            
+                let color = UIColor(hue: CGFloat(hue)/10, saturation: CGFloat(sat)/10, brightness: 1, alpha: 1)
+                
+                self.colors.append(color)
+            }
+        }
+    }
+    
+    // Mark: Collection View Data Source
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return self.colors.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+        
+        let color = self.colors[indexPath.row]
+        cell.backgroundColor = color
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 5
+        
+        return cell
     }
 }
 
